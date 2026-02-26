3d90a59ad5a62c1a6475390cc1cf6438fd589f2e
omarelhorre <elhorre.omar@etu.uae.ac.ma>
2026-02-26 22:46:47 +0000

    Finished readDot() function

--- header.c
+++ header.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define A[20][20]
-void readDot(void)
+#define MAX 20
+void readDot(int A[20][20])
 	{
 	FILE* f;
 	fptr = fopen("graph.dot","r");
 	if(fptr == NULL)
 		{
-			printf("File not opened, please check if your graph.dot file exists in this 					directory");
+			printf("File not opened, please check if your graph.dot file exists in this directory");
 		}
-	fscanf(file, "
-	while( fscanf(f," %c -> %c [label=\"%d\"]",&src,&dest,&val)
+	fscanf(file, "%s");
+	while( fscanf(f," %c -> %c [label=\"%d\"]",&src,&dest,&val ) == 3)
+			{
+				A[src][dest] = 1;
+			}
 	}
